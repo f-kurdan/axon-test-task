96f28c0a6e84b364e77fde2d19cbb7452d3ea82f
Kurdanov Feytullah <fkurdan@gmail.com>
2024-03-02 10:25:20 +0300

    Update dropdown

--- src/components/common/dropdown.tsx
+++ src/components/common/dropdown.tsx
@@ -1,22 +1,37 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import { UseFormRegister, UseFormSetValue } from 'react-hook-form/dist/types';
 
 import { Inputs } from '../../types/inputs';
 import Arrow from './arrow';
 
-const Dropdown = ({register, setValue, packageType}:{
-    register: UseFormRegister<Inputs>, 
+const Dropdown = ({ register, setValue, packageType }: {
+    register: UseFormRegister<Inputs>,
     setValue: UseFormSetValue<Inputs>,
-    packageType: string}) => {
+    packageType: string
+}) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selected, setSelected] = useState(packageType);
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         register('packageType',
-          { required: true}
+            { required: true }
         );
-      }, [register]);
+    }, [register]);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        }
+
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        }
+    }, [isOpen])
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
@@ -28,11 +43,9 @@ const Dropdown = ({register, setValue, packageType}:{
     }
 
     return (
-        <div className='create-container__input' onClick={toggleDropdown}>
-            <div onClick={toggleDropdown}>
-                {selected}
-                <div className={`create-container__arrow ${isOpen ? 'open' : ''}`}>{<Arrow />}</div>
-            </div>
+        <div ref={dropdownRef} className='create-container__input pointer' onClick={toggleDropdown}>
+            {selected}
+            <div className={`create-container__arrow ${isOpen ? 'open' : ''}`}>{<Arrow />}</div>
             {isOpen && (
                 <ul className='create-container__dropdown-list'>
                     <li value={'компрессия'} onClick={() => handleOptionClick('компрессия')}>компрессия</li>
